3eab9babddd9c3f11dcf165bf4c2a9595d2f0bbc
Diego Fernando Rojas <rojastorrado@gmail.com>
2022-10-06 23:36:22 -0500

    Handle optional logs path file

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,25 @@ use std::io::BufReader;
 use std::io::prelude::*;
 use std::io;
 
-pub fn read_file(logs_path: &PathBuf, mut writer: impl std::io::Write) -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::open(logs_path)?;
-    // Rust by default handles 3KB of capacity. We set 10KB.
-    // TODO: Handles this dinamically.
-    let mut content = BufReader::with_capacity(10, file);
-    let mut lines = String::new();
-    content.read_to_string(&mut lines)?;
-    writeln!(writer, "{}", lines)?;
+type ReturnedValue = Result<(), Box<dyn std::error::Error>>;
+
+pub fn handle_with_file(logs_path: Option<&PathBuf>, mut writer: impl std::io::Write) -> ReturnedValue {
+    match logs_path {
+        Some(logs_path) => { 
+            let file = File::open(logs_path)?;
+            // Rust by default handles 3KB of capacity. We set 10KB.
+            // TODO: Handles this dinamically.
+            let mut content = BufReader::with_capacity(10, file);
+            let mut lines = String::new();
+            content.read_to_string(&mut lines)?;
+            writeln!(writer, "{}", lines)?;
+        },
+        None => { println!("Nothing"); }
+    }
     Ok(())
 }
 
-pub fn handle_user_input() {
+pub fn handle_with_manual_inputs() {
     let mut number = String::new();
     println!("Enter any number");
     io::stdin()
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod lib;
 use clap::Parser;
-pub use crate::lib::{read_file, handle_user_input};
+pub use crate::lib::{handle_with_file, handle_with_manual_inputs};
 
 #[derive(Parser)]
 #[clap(author = "Diego Fernando Rojas", version, about)]
 struct Cli {
     // TODO: Implement short and long
-    vendor: Option<String>, // This has to be an Enum later.
+    vendor: String, // This has to be an Enum later.
     logs_path: Option<std::path::PathBuf>,
 }
 
@@ -15,10 +15,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // If there is only one element, means that there was no arguments passed.
     // The unique element is the name of the program.
     if get_args.len() == 1 {
-        openwater::handle_user_input();
-        Ok(());
+        openwater::handle_with_manual_inputs();
     }
     let args = Cli::parse();
-    openwater::read_file(&args.logs_path, &mut std::io::stdout())?;
-    Ok(());
+    openwater::handle_with_file(args.logs_path.as_ref(), &mut std::io::stdout())?;
+    Ok(())
 }
