db1e2b9044124f9032560d12080316efb21a87d8
Diego Fernando Rojas <rojastorrado@gmail.com>
2022-11-24 18:49:28 -0500

    Test

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,58 @@
+#![allow(unused_imports)]
+// https://blog.devgenius.io/reading-and-writing-a-json-file-in-rust-2731da8d6ad0
 #[macro_use]
-//mod lib;
-//pub use crate::lib::{handle_with_file, handle_with_manual_inputs};
 mod args;
 
-use args::{OpenWaterArgs, ManualCommand};
+// use args::{OpenWaterArgs, ManualCommand, InputType};
+use args::{OpenWaterArgs};
 use clap::Parser;
-//use serde::{Serialize, Deserialize};
+use serde_json;
 use serde_json::{Result, Value};
 
+use serde::{Deserialize, Serialize};
+
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Serialize, Deserialize)]
+struct Address {
+    street: String,
+    city: String,
+}
 
 
-fn main() { //-> Result<(), Box<dyn std::error::Error>> {
-    let args : OpenWaterArgs = OpenWaterArgs::parse();
-    let p: ManualCommand = serde_json::from_str(args);
-    println!("{:?}", p);
+fn main() -> Result<()> {
+    //let _args : OpenWaterArgs = OpenWaterArgs::parse();
+    // test
+    //println!("{:?}", 123);
+
+    let mut file = File::open("src/my-dives.ssrf").expect("File not found JJAJA");
+    let mut data = String::new();
+    file.read_to_string(&mut data).expect("Error while reading file");
+    println!("{}", data);
+
+    // args.input_type {
+    //     InputType::Manual(user) => "Hola",
+    // };
+    
+    // println!("{:?}", args.);
+
+    // let m = ManualCommand {
+    //     //date: String::from("someone@example.com"),
+    //     //depth: String::from("someone@example.com")
+    //     Box::new(args::ManualCommand)
+    // };
+
+    // let address = Address {
+    //     street: "10 Downing Street".to_owned(),
+    //     city: "London".to_owned(),
+    // };
+
+    // let j = serde_json::to_string(&address)?;
+
+    // // println!("{:?}", j);
+    // println!("{}", j);
+
+
+    Ok(())
 }
